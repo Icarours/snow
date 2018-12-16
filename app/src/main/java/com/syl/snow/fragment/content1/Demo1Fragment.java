be11e50f5fdc2d65dc04db8bafa5d15524809059
j376787348 <j376787348@163.com>
2018-12-17 00:10:29 +0800

    Activity向Fragment传递数据

--- app/src/main/java/com/syl/snow/fragment/content1/Demo1Fragment.java
+++ app/src/main/java/com/syl/snow/fragment/content1/Demo1Fragment.java
@@ -19,7 +19,13 @@ import butterknife.ButterKnife;
  * Created by Bright on 2018/12/16.
  *
  * @Describe content1跳转过来后对应的空白Fragment
- * Activity向Fragment传递数据
+ * Activity向Fragment传递数据,最好使用Fragment.setArguments(Bundle bundle)传递参数,不要使用在构造方法中传递参数的方法
+ * <p>
+ * 根据Android文档说明，当一个Fragment重新创建的时候，系统会再次调用Fragment中的默认构造函数，
+ * 注意是默认构造函数。即，当你创建了一个带有参数的Fragment的之后，一旦由于什么原因
+ * （例如横竖屏切换）导致你的Fragment重新创建。那么，很遗憾，你之前传递的参数都不见了，
+ * 因为recreate你的Fragment的时候，调用的是默认构造函数。因此，官方推荐使用
+ * Fragment.setArguments(Bundle bundle)这种方式来传递参数，而不推荐通过构造方法直接来传递参数。
  * @Called
  */
 public class Demo1Fragment extends BaseFragment {
